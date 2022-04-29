2e8493be638b08053aa5fe354754c4ee9704623f
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-04-29 17:10:40 -0300

    arquitetura inicial

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-import dotenv  from "dotenv"
+import dotenv  from "dotenv";
 import express from "express";
 import path from "path";
 
 dotenv.config();
+
+const __dirname = path.resolve(path.dirname(""));
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
